8ad0692de3ded4bf47b71c359bd0aabdc89a9416
Geice Sousa Pinho <geicesousa@outlook.com>
2023-10-23 23:51:37 -0300

    inicio dos exercicios

---
+++ exercicios/para-casa/entregas/Geice-SousaPinho/exercicio2.test.js
@@ -0,0 +1,3 @@
+// - Exercício 2 - Cenários e casos de teste:
+// Escreva os cenários de teste para o [habitica](https://habitica.com/static/home), considere pelo menos duas funcionalidades descritas no [Step 1 da Wiki do Habitica](https://habitica.fandom.com/wiki/Habitica_Wiki). Os cenários de teste não precisam ser completos, mas devem contemplar as principais fluxos da funcionalidade.
+
