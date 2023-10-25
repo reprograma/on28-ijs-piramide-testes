7b4dd5252833968036b987188e3c3dff22321aec
Geice Sousa Pinho <geicesousa@outlook.com>
2023-10-25 02:04:06 -0300

    add: exercicio1 finalizado

--- exercicios/para-casa/entregas/Geice-SousaPinho/exercicio1.test.js
+++ exercicios/para-casa/entregas/Geice-SousaPinho/exercicio1.test.js
@@ -1,17 +1,20 @@
 // - Exercício 1 - API de localidades (IBGE): 
 // Teste pelo menos 4 endpoints da [API de localidades do IBGE](https://servicodados.ibge.gov.br/api/docs/localidades), cobrindo caminhos felizes e infelizes. Exercite sua autonomia no aprendizado e fique a vontade para usar diferentes matchers do jest e acessar diferentes respostas com o supertest.
 
+const { Console } = require('console');
 const request = require('supertest');
 const url = 'https://servicodados.ibge.gov.br/api/v1/localidades/'
 
 describe('API IBGE', ()=>{
-    test('deveria retornar uma região de acordo com o id', async ()=>{
+    test('deveria retornar uma REGIÃO de acordo com o id', async ()=>{
+        const response = await request(url).get('regioes/')
         const response1 = await request(url).get('regioes/1')
         const response2 = await request(url).get('regioes/2')
         const response3 = await request(url).get('regioes/3')
         const response4 = await request(url).get('regioes/4')
         const response5 = await request(url).get('regioes/5')
     
+        expect(response.body.nome).toBeUndefined()
         expect(response1.body.nome).toBe('Norte')
         expect(response1.body.sigla).toBe('N')
         expect(response2.body.nome).toBe('Nordeste')
@@ -24,12 +27,26 @@ describe('API IBGE', ()=>{
         expect(response5.body.sigla).toBe('CO')
     });
 
-    test('deveria retorna um estado de acordo com o id', ()=>{
-        const response = request(url).get('estados/33')
-        const responseBahia = request(url).get('estados/29')
+    test('deveria retornar um ESTADO de acordo com o id', async ()=>{
+        const response = await request(url).get('estados/33')
+        const responseBahia = await request(url).get('estados/29')
 
         expect(response.body.nome).toEqual('Rio de Janeiro')
-        expect(response.nome).toBeTruthy()
-        // expect(responseBahia.body.nome).toBe('Bahia')
+        expect(responseBahia.body.nome).toEqual('Bahia')
     });
+
+    test('deveria retornar um PAÍS', async ()=>{
+        const response = await request(url).get('paises/')
+
+        console.log(response.body)
+        expect(response.body).toBeDefined()
+        expect(response.body).toBeTruthy()
+    })
+
+    test('deveria retornar a quantidade de REGIÕES METROPOLITANAS de acordo com id da UF', async ()=>{
+        const response = await request(url).get('estados/29/regioes-metropolitanas')
+
+        expect(response.body).toHaveLength(3)
+    })
+    
 })
